31ada62647f1088d1e8d635b4ed9e7e7c7ded52d
SHShadhin <shshadhin0@gmail.com>
2026-03-13 22:49:26 +0600

    different ways to add event handler in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,34 @@
 import './App.css'
 
 function App() {
+  function handleClick() {
+    alert('Clicked')
+  }
+
+  const handleClick3 = () => {
+    alert('Clicked 3')
+  }
+
+  const handleAdd5=(num)=> {
+    const newNum = num + 5;
+    alert(newNum);
+  }
 
   return (
     <>
       <div>
-        <h1>Get started</h1>
+        <h1>Get started React</h1> 
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={function handleClick2() {
+          alert('Clicked 2')
+        }}>Click Me 2</button>
+
+        <button onClick={handleClick3}>Click Me 3</button>
+        <button onClick={ () => {
+          alert('click 4')
+        }}>Click Me 4</button>
+
+        <button onClick={()=> handleAdd5(10)}>Add 5</button>
       </div>
     </>
   );
