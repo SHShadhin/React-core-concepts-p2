169a992830be9a4f2d3ff8a413368afb57c3158d
SHShadhin <shshadhin0@gmail.com>
2026-03-14 13:00:51 +0600

    load dynamic data api call using use method

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import Batsman from './Batsman';
 import './App.css'
 import Counter from './Counter';
+import Users from './Users';
+import { Suspense } from 'react';
+import Friends from './Friends';
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users').then(
+//   res => res.json(),
+// );
+const fetchFriends = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json()
+}
+
+
 function App() {
+
+  const friendsPromise = fetchFriends();
+
   function handleClick() {
     alert('Clicked')
   }
@@ -15,25 +31,43 @@ function App() {
     alert(newNum);
   }
 
+  
+  
   return (
     <>
       <div>
-        <h1>Get started React</h1> 
+        <h1>Get started React</h1>
+
+        {/* <Suspense fallback={<h2>Loading...</h2>}>
+          <Users fetchUsers={fetchUsers}></Users>
+        </Suspense> */}
+
+        <Suspense fallback={<h2>Friends are coming for treat</h2>}>
+          <Friends friendsPromise={friendsPromise}></Friends>
+        </Suspense>
+
         <Batsman></Batsman>
         <Counter></Counter>
 
-
         <button onClick={handleClick}>Click Me</button>
-        <button onClick={function handleClick2() {
-          alert('Clicked 2')
-        }}>Click Me 2</button>
+        <button
+          onClick={function handleClick2() {
+            alert('Clicked 2');
+          }}
+        >
+          Click Me 2
+        </button>
 
         <button onClick={handleClick3}>Click Me 3</button>
-        <button onClick={ () => {
-          alert('click 4')
-        }}>Click Me 4</button>
+        <button
+          onClick={() => {
+            alert('click 4');
+          }}
+        >
+          Click Me 4
+        </button>
 
-        <button onClick={()=> handleAdd5(10)}>Add 5</button>
+        <button onClick={() => handleAdd5(10)}>Add 5</button>
       </div>
     </>
   );
